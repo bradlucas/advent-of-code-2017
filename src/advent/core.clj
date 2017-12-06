6a1dead46b9b9fc409eea828bbf0339e7bad9f69
Brad Lucas <brad@beaconhill.com>
2017-12-06 16:14:27 -0500

    day2

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,6 +1,8 @@
 (ns advent.core
   (:require [advent.day1.captcha :as day1-part1]
-            [advent.day1.captcha2 :as day1-part2])
+            [advent.day1.captcha2 :as day1-part2]
+            [advent.day2.checksum :as day2]
+            )
   (:gen-class))
 
 
@@ -10,4 +12,6 @@
   (println (format "Day 1 / Part 1 : %d" (day1-part1/run)))
   (println (format "Day 1 / Part 2 : %d" (day1-part2/run)))
 
+  (println (format "Day 2 / Part 1 : %d" (day2/run-part1)))
+  (println (format "Day 2 / Part 2 : %d" (day2/run-part2)))
   )
